e3d0b37cbabf11ec50451aa34e727973d79bbe71
Alex Nascimento <alexjose68@hotmail.com>
2024-06-10 13:33:01 -0300

    feat: instalei o react-icons e importei um icone de carrinho de compras para o Header e estilizei o componente  minimamente

--- src/components/header/index.tsx
+++ src/components/header/index.tsx
@@ -1,7 +1,21 @@
+import { Link } from 'react-router-dom';
+import { FiShoppingCart } from 'react-icons/fi';
+
 export function Header() {
   return(
-    <div>
-      <h1>HEADER COMPONENT</h1>
-    </div>
+    <header className='w-full px-1 bg-slate-200'>
+      <nav className='w-full max-w-7xl h-14 flex items-center justify-between px-5 mx-auto'>
+        <Link className='font-bold text-2xl' to="/">
+          Dev Shop
+        </Link>
+
+        <Link className='relative' to="cart">
+          <FiShoppingCart size={24} color='121212'/>
+          <span className='absolute -top-3 -right-3 bg-sky-500 rounded-full w-6 h-6 flex items-center justify-center text-white'>
+            2
+          </span>
+        </Link>
+      </nav>
+    </header>
   )
 }
