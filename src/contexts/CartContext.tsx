cc3b06fca223d736de857696531e308de68adba4
Alex Nascimento <alexjose68@hotmail.com>
2024-06-11 19:22:05 -0300

    feat: criei outros values para o CartContext dentre eles o que remove itens do carrinho de compras, também o que trata o total do passando a lógica necessária e passando o valor formatado em reais, passei esses valus para a página do carrinho também criando a lógica e estrutura HTML e estilizando o componente em si

--- src/contexts/CartContext.tsx
+++ src/contexts/CartContext.tsx
@@ -5,6 +5,8 @@ interface CartContextData {
   cart: CartProps[];
   cartAmount: number;
   addItemCart: (newItem: ProductsProps) => void;
+  removeItemCart: (product: CartProps) => void;
+  total: string;
 }
 
 interface CartProps {
@@ -25,6 +27,8 @@ export const CartContext = createContext({} as CartContextData)
 
 function CartProvider({children}: CartProviderProps) {
   const [cart, setCart] = useState<CartProps[]>([]);
+  
+  const [total, setTotal] = useState("");
 
   function addItemCart(newItem: ProductsProps) {
     // Adiciona no carrinho
@@ -39,17 +43,50 @@ function CartProvider({children}: CartProviderProps) {
       cartList[indexItem].total = cartList[indexItem].amount * cartList[indexItem].price;
 
       setCart(cartList);
+      // Para recalcular o total (após adicionar um item)
+      totalResultCart(cartList)
       return; // para parar a execução da função
     }
-    // Adicionar esse item na nossa lista.
 
+    // Adicionar esse item na nossa lista.
     let data = {
       ...newItem,
       amount: 1,
       total: newItem.price
     }
 
-    setCart(products => [...products, data])
+    setCart(products => [...products, data]);
+    // Para recalcular o total (após adicionar item)
+    totalResultCart([...cart, data]);
+  }
+
+  function removeItemCart(product: CartProps) {
+    const indexItem = cart.findIndex(item => item.id === product.id);
+
+    if(cart[indexItem]?.amount > 1) {
+      // Diminuir apenas 1 amount do que você tem
+      let cartList = cart;
+
+      cartList[indexItem].amount = cartList[indexItem].amount -1;
+      cartList[indexItem].total = cartList[indexItem].total - cartList[indexItem].price;
+
+      // Para recalcular a quantidade de itens após remover -1 da quantidade do item e também o total:
+      setCart(cartList);
+      totalResultCart(cartList);
+      return; // Para parar a execução da função
+    }
+
+    const removeItem = cart.filter(item => item.id !== product.id);
+    setCart(removeItem);
+    // Para recalcular o total (após remover um item):
+    totalResultCart(removeItem);
+  }
+
+  function totalResultCart(items: CartProps[]) {
+    let myCart = items;
+    let result = myCart.reduce((acc, obj) => { return acc + obj.total}, 0 );
+    const resultFormatted = result.toLocaleString("pt-BR", { style: "currency", currency: "BRL" });
+    setTotal(resultFormatted);
   }
 
   return (
@@ -57,7 +94,9 @@ function CartProvider({children}: CartProviderProps) {
       value={{
         cart,
         cartAmount: cart.length,
-        addItemCart
+        addItemCart,
+        removeItemCart,
+        total
       }}
     >
       {children}
--- src/pages/cart/index.tsx
+++ src/pages/cart/index.tsx
@@ -1,34 +1,67 @@
+import { useContext } from 'react';
+import { Link } from 'react-router-dom';
+import { CartContext } from '../../contexts/CartContext';
+
 export function Cart() {
+  const { cart, total, removeItemCart, addItemCart } = useContext(CartContext);
+
   return (
     <div className="wfull max-w-7xl mx-auto">
       <h1 className="font-medium text-2xl text-center my-4">Carrinho de compras</h1>
 
-      <section className="flex items-center justify-between border-b-2 border-gray-300">
+      {cart.length === 0 && (
+        <div className='flex flex-col items-center justify-center'>
+          <p className='font-medium'>Ops seu carrinho está vazio...</p>
+          <Link
+            to="/"
+            className='bg-slate-600 my-3 p-1 px-3 text-white font-medium rounded'
+          >
+            Acessar produtos
+          </Link>
+        </div>
+      )}
+
+      {cart.map( (item) => (
+        <section
+          key={item.id}
+          className="flex items-center justify-between border-b-2 border-gray-300"
+        >
         <img
-          src="https://media.karousell.com/media/photos/products/2023/11/30/ryo_sakazaki__storm_collectibl_1701369450_d2adf184_progressive"
-          alt="logo do produto"
+          src={item.cover}
+          alt={item.title}
           className="w-28"
         />
 
         <strong>Preço: R$ 1.000</strong>
 
         <div className="flex items-center justify-center gap-3">
-          <button className="bg-slate-600 px-2 rounded text-white font-medium flex items-center justify-center">
+          <button
+            onClick={ () => removeItemCart(item) }
+            className="bg-slate-600 px-2 rounded text-white font-medium flex items-center justify-center"
+          >
             -
           </button>
 
-          1
+          {item.amount}
 
-          <button className="bg-slate-600 px-2 rounded text-white font-medium flex items-center justify-center">
+          <button
+            onClick={ () => addItemCart(item) }
+            className="bg-slate-600 px-2 rounded text-white font-medium flex items-center justify-center"
+          >
             +
           </button>
         </div>
 
         <strong className="float-right">
-          SubTotal: R$ 1.000
+          SubTotal: {item.total.toLocaleString("pt-BR", {
+            style: "currency",
+            currency: "BRL"
+          })}
         </strong>
       </section>
-      <p className="font-bold mt-4">Total: R$ 1.000</p>
+      ))}
+      
+      {cart.length !== 0 && <p className="font-bold mt-4">Total: {total}</p>}
     </div>
   )
 }
